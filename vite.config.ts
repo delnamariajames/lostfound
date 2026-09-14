80a5e6f12a31c09481ac350dba89eec4b6e60923
delnamariajames <jamesdelnamaria@gmail.com>
2026-09-15 00:44:29 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -83,7 +83,7 @@ function aistudioMediaPlugin(): Plugin {
 
 export default defineConfig(() => {
   return {
-   // base: '/lostfound/',
+    base: process.env.GITHUB_ACTIONS ?
     plugins: [react(), tailwindcss(), expressApiPlugin(), aistudioMediaPlugin()],
     resolve: {
       alias: {
